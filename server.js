33523c78e87f4380ebc8fa304384efd9e4a1ca2e
Glitch (hello-express) <none>
2021-06-15 01:51:36 +0000

    🍕😺 Checkpoint ./views/index.html:3138560/3246 ./server.js:3138560/204

--- server.js
+++ server.js
@@ -4,14 +4,20 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/login", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
