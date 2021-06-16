247cb1533fbbdfaf886a7bc620bef3face05219f
Glitch (hello-express) <none>
2021-06-16 11:46:46 +0000

    ⏳👶 Checkpoint ./server.js:3138560/601 ./views/otp.html:3138560/162

--- server.js
+++ server.js
@@ -39,9 +39,6 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/otp", (request, response) => {
-  response.sendFile(__dirname + "/views/otp.html");
-});
 
 app.post("/home", async (request, response) => {
   var username = request.body.username;
@@ -94,7 +91,7 @@ app.post("/home", async (request, response) => {
         }
         console.log('In select-authenticator-authenticate');
         var authTransactionEmail = await authClient.idx.authenticate({ authenticator: 'email' });
-        response.sendFile(__dirname + "/views/otp.html");
+        response.render('otp.html', {"otp_text": "Enter the OTP you received to authenticate"});
       } else {
         authClient.transactionManager.clear();
         response.redirect("https://oie-buildathon.glitch.me?error=Invalid Credentials");
@@ -134,7 +131,7 @@ app.post("/verifyotp", async (request, response) => {
     response.render('home.html', {"name": name});
   } else {
     console.log("Incorrect OTP: " + otp);
-    response.redirect('/otp?error=invalid otp');
+    response.render('otp.html', {"otp_text": "You entered an incorrect OTP. Try again"});
   }
 });
 
